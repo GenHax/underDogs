409929066a3d99592391638f6adaf7cd131f9d28
Ashis kumar <ashishsahoo389@gmail.com>
2018-08-19 00:00:14 +0530

    Files Added

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { AgmCoreModule } from '@agm/core';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { MainPage } from '../pages/main/main';
+
+
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    MainPage
+   
+
+  ],
+  imports: [
+    BrowserModule,
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDT6Wstz31NrcHRdWPVtEQEVkPA6NDz8Mg'})
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    MainPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { MainPage } from '../main/main';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+  
+    }
+    goMain() {
+      this.navCtrl.push(MainPage);
+    }
+ 
+  }
+
+
